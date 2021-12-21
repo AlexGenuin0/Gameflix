85aa977efe836595967d8e3bffeed998cb432d38
Alex <60670459+AlexGenuin0@users.noreply.github.com>
2021-12-21 00:01:51 -0300

    Add files via upload

---
+++ js/owl/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 70,
+    loop:true,
+    margin:7,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
